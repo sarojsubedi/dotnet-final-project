49ec93f83c07f4279455f95e2f031885fa3b0e77
sarojsubedi <sarojsubedi@pm.me>
2026-06-09 09:10:44 +0545

    added config for db

--- Hrs.Api.Repository/Data/HrsDbContext.cs
+++ Hrs.Api.Repository/Data/HrsDbContext.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Hrs.Api.Repository.Data;
 
 public class HrsDbContext
 {
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Author { get; set; }
+
+    }
     
 }
---
+++ Hrs.Api.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Hrs.Api.Shared;
+
+public class Class1
+{
+}
